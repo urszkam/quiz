a8c627163f61326596a430be4d584e52ee03db7d
jillg7988 <115293607+jillg7988@users.noreply.github.com>
2023-04-02 11:46:17 -0400

    Body of instructions page

--- src/pages/instructions.js
+++ src/pages/instructions.js
@@ -4,7 +4,50 @@ const Instructions = () => {
   return (
     <div>
       <h1>Page with instructions</h1>
+
+      <body>
+    <h1>RULES & REGULATIONS</h1>
+
+    <h2>NOTE:</h2>
+
+      <p1>
+        1. The amount of questions picked from the starting page cannot be changed after starting the quiz.
+        In order to change the amount of questions, please return to the main page and change the amount to your liking.
+      </p1>
+
+        <p2>
+        2. The qustions will be randomly selected and will strictly revole around the ocean life and the organisms living therein.
+       </p2>
+
+        <p3>
+        3. Feel free to look up the answers, no judging from our end.
+        Google is always lovely but try these sources at the bottom of the page for a better grasp of the topic.
+        </p3>
+
+        <p4>
+        4. Have fun guessing. You may be shocked by how lucky you actually are or by how much you actually know.
+      </p4>
+
+        <p5>
+        INPUT 'HOME' button on LEFT and 'START' button on RIGHT 
+      </p5> 
+
+
+    <h1>SOURCES:</h1>
+        <p6>
+        SOURCE #1
+        </p6>
+
+        <p7>
+        SOURCE #2
+        </p7>
+
+        <p8>
+        SOURCE #3
+        </p8>
+    </body>
     </div>
+
   );
 };
   
