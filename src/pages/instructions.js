db6cb986dbb33bb56a8d0bf06693c186c640e931
Ula Kaminska <ursz.kam@gmail.com>
2023-04-04 15:41:48 +0200

    delete unnecessary tags

--- src/pages/instructions.js
+++ src/pages/instructions.js
@@ -21,6 +21,7 @@ import { useLocation, useNavigate } from "react-router-dom";
     const returnHome = () => {
       navigate("/")
     }
+
     return (
     <div className="Instructions-page">
       
@@ -28,8 +29,7 @@ import { useLocation, useNavigate } from "react-router-dom";
       <h1 id="title1">Instructions</h1>
       </header>
 
-      <body className="Instructions-body">
-      <p1 id="body1">
+      <div className="Instructions-body">
         <p>
         <b>1.</b> Make sure that you're comfortable with your choice in difficulty and number of questions. 
         </p>
@@ -52,108 +52,71 @@ import { useLocation, useNavigate } from "react-router-dom";
         <b>4.</b> Have fun guessing! You may be shocked by how lucky you actually are or by how much you actually know.
         </p>
 
-        <p>
         <div className="buttonChoice__container">
           <NextBtn action={handleClick}
             id_div={"startDiv"}
             id_btn={"startBtn"}/>
         </div>
-        </p>
-        </p1>
-      </body>
-
-      <body className="Sources-body">
-      <h2 id="title2">HELPFUL SOURCES:</h2>
-      <p id="sourceParas">
-      <p>
-        <a
+        <h2 id="title2">HELPFUL SOURCES:</h2>
+          <a
           className="Instructions-link"
           href="https://www.britannica.com/topic/Panama-Canal"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           PANAMA CANAL by Norman J. Padelford et. al. for Encyclopedia Britannica, March 3, 2023.
         </a>
-      </p>
-      <p>Said literally no one: I yearn to know what <em>oceans</em> are connected by the Panama Canal!</p>
-      <br></br>
-      <p>
+        <p>Said literally no one: I yearn to know what <em>oceans</em> are connected by the Panama Canal!</p>
         <a
           className="Instructions-link"
           href="https://www.worldwildlife.org/stories/unicorn-of-the-sea-narwhal-facts"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           UNICORN OF THE SEA: Narwhal Facts by WorldWildLife
         </a>
-      </p>
-      <p> Unicorn of the Sea!!! Don't even look like unicorns! And instead of unicorn horns, they have <em>tusks</em>. What a sham?!?</p>
-      <br></br>
-      <p>
+        <p> Unicorn of the Sea!!! Don't even look like unicorns! And instead of unicorn horns, they have <em>tusks</em>. What a sham?!?</p>
         <a
           className="Instructions-link"
           href="https://www.britannica.com/animal/walrus"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           WALRUS by the Editors of Encyclopaedia for Encyclopedia Britannica, 18 Feb. 2023.
         </a>
-      </p>
-      <p>One nickname sounds like something out of Harry Potter! So where's the creativity in <em>the other one</em>?!?</p>
-      <br></br>
-      <p>
+        <p>One nickname sounds like something out of Harry Potter! So where's the creativity in <em>the other one</em>?!?</p>
         <a
           className="Instructions-link"
           href="https://oceanconservancy.org/blog/2019/06/05/biggest-animals-ocean/"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Who are the Biggest Animals in the Ocean?
         </a>
-      </p>
-      <p>Mirror, mirror! On the wall! Who's the <em>largest</em> of them all?</p>
-      <br></br>
-      <p>
+        <p>Mirror, mirror! On the wall! Who's the <em>largest</em> of them all?</p>
         <a
           className="Instructions-link"
           href="https://www.history.com/this-day-in-history/magellans-expedition-circumnavigates-globe"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Magellan’s Expedition
         </a>
-      </p>
-      <p>I was looking for a word that <em>rhymes with euphoria</em>!!!</p>
-      <br></br>
-      <p>
+        <p>I was looking for a word that <em>rhymes with euphoria</em>!!!</p>
         <a
           className="Instructions-link"
           href="https://www.livescience.com/55412-whale-sharks.html"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Facts About Whale Sharks By Alina Bradford
         </a>
-      </p>
-      <p>It's about the size of a WHAT!</p>
-      <br></br>
-      <p>
+        <p>It's about the size of a WHAT!</p>
         <a
           className="Instructions-link"
           href="https://www.bbc.com/news/science-environment-22715018"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           How the turtle got its unique hard shell By Melissa Hogenboom, Science reporter, BBC News
         </a>
-      </p>
-      <p>That's <em>a lot</em> of bones!</p>
-      <br></br>
-      </p>
-      <ReturnBtn 
-        action={returnHome}/>
-      </body>
-
+        <p>That's <em>a lot</em> of bones!</p>
+        <ReturnBtn 
+          action={returnHome}/>
+        </div>
     </div>
 
   );
