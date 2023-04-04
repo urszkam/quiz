222dfe572115e27666ac4a3605804982a3772a36
jillg7988 <115293607+jillg7988@users.noreply.github.com>
2023-04-03 21:09:29 -0400

    Final amendments!

--- src/pages/instructions.js
+++ src/pages/instructions.js
@@ -32,25 +32,25 @@ import { useLocation, useNavigate } from "react-router-dom";
       <body className="Instructions-body">
       <p1 id="body1">
         <p>
-        1. Make sure that you're comfortable with your choice in difficulty and number of questions. 
+        <b>1.</b> Make sure that you're comfortable with your choice in difficulty and number of questions. 
         </p>
 
         <p>
-        NOTE: The difficulty and number of questions chosen from the starting page cannot be changed after starting the quiz.
-        In order to make changes, use the 'RETURN' button on the bottom of the page to return to the main page and change the amount to your liking.
+        <b>NOTE:</b> The difficulty and number of questions chosen from the starting page cannot be changed after starting the quiz.
+        In order to make changes, use the <b>'RETURN'</b> button on the bottom of the page to return to the main page and change the amount to your liking.
+        </p>
+        
+        <p>
+        <b>2.</b> The qustions will be randomly selected and will strictly revolve around the ocean life and the organisms living therein.
         </p>
 
         <p>
-        2. The qustions will be randomly selected and will strictly revolve around the ocean life and the organisms living therein.
+        <b>3.</b> Feel free to look up the answers, no judging from our end.
+        Google is always lovely but try these sources at the bottom of the page for hints on the answers to some of the questions.
         </p>
 
         <p>
-        3. Feel free to look up the answers, no judging from our end.
-        Google is always lovely but try these sources at the bottom of the page for some hints on the answers to some questions.
-        </p>
-
-        <p>
-        4. But.... have fun guessing. You may be shocked by how lucky you actually are or by how much you actually know.
+        <b>4.</b> Have fun guessing! You may be shocked by how lucky you actually are or by how much you actually know.
         </p>
 
         <p>
@@ -75,7 +75,7 @@ import { useLocation, useNavigate } from "react-router-dom";
         </a>
       </p>
       <p>Said literally no one: I yearn to know what <em>oceans</em> are connected by the Panama Canal!</p>
-
+      <br></br>
       <p>
         <a
           className="Instructions-link"
@@ -87,7 +87,7 @@ import { useLocation, useNavigate } from "react-router-dom";
         </a>
       </p>
       <p> Unicorn of the Sea!!! Don't even look like unicorns! And instead of unicorn horns, they have <em>tusks</em>. What a sham?!?</p>
-
+      <br></br>
       <p>
         <a
           className="Instructions-link"
@@ -99,7 +99,7 @@ import { useLocation, useNavigate } from "react-router-dom";
         </a>
       </p>
       <p>One nickname sounds like something out of Harry Potter! So where's the creativity in <em>the other one</em>?!?</p>
-
+      <br></br>
       <p>
         <a
           className="Instructions-link"
@@ -111,7 +111,7 @@ import { useLocation, useNavigate } from "react-router-dom";
         </a>
       </p>
       <p>Mirror, mirror! On the wall! Who's the <em>largest</em> of them all?</p>
-
+      <br></br>
       <p>
         <a
           className="Instructions-link"
@@ -123,7 +123,7 @@ import { useLocation, useNavigate } from "react-router-dom";
         </a>
       </p>
       <p>I was looking for a word that <em>rhymes with euphoria</em>!!!</p>
-
+      <br></br>
       <p>
         <a
           className="Instructions-link"
@@ -135,7 +135,7 @@ import { useLocation, useNavigate } from "react-router-dom";
         </a>
       </p>
       <p>It's about the size of a WHAT!</p>
-
+      <br></br>
       <p>
         <a
           className="Instructions-link"
